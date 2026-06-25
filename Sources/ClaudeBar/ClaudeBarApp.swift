70d17f1a3ca301ae064efc34bf4eff47e7a360e6
Nicolas Erlichman <nicolas@gogrow.dev>
2026-06-25 12:04:51 -0300

    Bump version to 0.1.2

--- Sources/ClaudeBar/ClaudeBarApp.swift
+++ Sources/ClaudeBar/ClaudeBarApp.swift
@@ -8,7 +8,7 @@ struct ClaudeBarApp: App {
 
     init() {
         Self.enforceSingleInstance()
-        Log.info("ClaudeBar launched (version 0.1.1)")
+        Log.info("ClaudeBar launched (version 0.1.2)")
         appState = AppState.shared
         appState.start()
     }
